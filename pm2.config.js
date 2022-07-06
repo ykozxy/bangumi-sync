563b7a701650d86937803032489a8abc30436851
Xuyang Zhou <nick.zhouxy@gmail.com>
2022-07-06 13:44:27 -0700

    Watch file changes in server mode
    Added config file to pm2 to watch and restart server upon file changes.

---
+++ pm2.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    apps: [{
+        name: "bangumi-sync",
+        script: "./build/main.js",
+        args: "--server",
+        watch_delay: 1000,
+        watch: ["./build", "./ignore_entries.json", "./manual_relations.json"],
+    }]
+}
