bae37f5c0d527b810632d2e2c20240881f97258a
Xuyang Zhou <nick.zhouxy@gmail.com>
2022-07-06 21:08:29 -0700

    Revert pm2 --watch option

--- pm2.config.js
+++ pm2.config.js
@@ -3,7 +3,8 @@ module.exports = {
         name: "bangumi-sync",
         script: "./build/main.js",
         args: "--server",
-        watch_delay: 1000,
-        watch: ["./build", "./ignore_entries.json", "./manual_relations.json"],
+        // watch_delay: 1000,
+        // watch: ["build", "./ignore_entries.json", "./manual_relations.json", "./config.json"],
+        // ignore_watch: ["node_modules", "cache", "log"]
     }]
 }
